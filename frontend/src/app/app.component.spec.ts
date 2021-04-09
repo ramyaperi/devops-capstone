498119205b5a2a411cd48d70280e8cf7153c9874
ramp <ramya.peri@top-itservices.com>
2021-04-09 21:15:37 +0200

    init

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('frontend app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Hello from me to you');
   });
 });
